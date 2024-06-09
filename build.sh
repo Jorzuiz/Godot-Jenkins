01b3656b71ebad409e9a5beef9d343ebd4e1802c
Jorge Zurdo <jordazurdo@hotmail.com>
2024-06-09 18:54:41 +0200

    Import Files

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 # Windows build
 
-./Godot_v4.2.2-stable_win64_console.exe --headless --path "First Person Shooter Template" --export-release "Windows Desktop" "Build_Windows/MyGame.exe"
+PROJECT_NAME="Jueguico"
+BUILD_DIR="./builds/$PROJECT_NAME"
+GODOT_PATH="/path/to/godot"
+
+# Limpia la build anterior
+rm -rf $BUILD_DIR
+#Crea directorio para la nueva build
+mkdir -p $BUILD_DIR
+
+./Godot_v4.2.2-stable_win64_console.exe --headless --path "First Person Shooter Template" --export-release "Windows Desktop" "../$BUILD_DIR/MyGame.exe"
+
+echo "Build correct"
